0274b5fb9be1f91593cce0e9909d7c76a9db46f1
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-02 13:02:12 -0500

    Wheel component is successfuly connected to the clockwise and counterclockwise action creators and reducer

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,28 +1,62 @@
+import { 
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM
+ } from './action-types'; 
+
+ import axios from 'axios';
+
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() {
+  return { type: MOVE_CLOCKWISE }
+}
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() { 
+  return { type: MOVE_COUNTERCLOCKWISE }
+}
 
-export function selectAnswer() { }
+export function selectAnswer(answer) { 
+  return { type: SET_SELECTED_ANSWER, payload: answer }
+}
 
-export function setMessage() { }
+export function setMessage(message) { 
+  return { type: SET_INFO_MESSAGE, payload: message }
+}
 
-export function setQuiz() { }
+export function setQuiz(data) { 
+  return { type: SET_QUIZ_INTO_STATE, payload: data }
+}
 
-export function inputChange() { }
+export function inputChange() { 
+  return { type: INPUT_CHANGE }
+}
 
-export function resetForm() { }
+export function resetForm() { 
+  return { type: RESET_FORM }
+}
 
 // ❗ Async action creators
 export function fetchQuiz() {
   return function (dispatch) {
+    dispatch(setQuiz());
+    axios.get('http://localhost:9000/api/quiz/next')
+      .then(({data}) => {
+        dispatch(setQuiz());
+      })
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
   }
 }
-export function postAnswer() {
+export function postAnswer(answer) {
   return function (dispatch) {
+    axios.post('http://localhost:9000/api/quiz/answer', answer)
+      .then()
+      .then()
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,9 +1,34 @@
 // ❗ You don't need to add extra reducers to achieve MVP
+import { 
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM
+} from './action-types';
+
 import { combineReducers } from 'redux'
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
+  switch(action.type) {
+    case MOVE_CLOCKWISE:
+      if (state === 5) {
+        return state = initialWheelState;
+      } else {
+        return state + 1;
+      }
+    case MOVE_COUNTERCLOCKWISE:
+      if (state === 0) {
+        return state = 5;
+      } else {
+        return state - 1;
+      }
+    default:
+      return state;
+  }
 }
 
 const initialQuizState = null
